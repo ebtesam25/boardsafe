28ea4391027cc89b94ee4c0f86ac8e250627da70
ebtesam25 <ebtesam@iut-dhaka.edu>
2020-07-04 09:36:13 +0600

    web-app initial commit

---
+++ web-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '.assets/fonts/Gilmer.otf';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 style={{fontFamily:'Gilmer'}}>Landing Page</h2>
+    </div>
+  );
+}
+
+export default App;
